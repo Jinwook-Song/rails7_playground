e61bed2fbbb842adbd1c5cb6ca5490905644ef84
jinwook song <wlsdnr129@naver.com>
2025-06-23 23:35:34 +0900

    feat(query): get query string

--- basics/app/controllers/home_controller.rb
+++ basics/app/controllers/home_controller.rb
@@ -1,5 +1,21 @@
 class HomeController < ApplicationController
     def index
-        render json: { message: "Hello, World!" }
+        # render html: "<h1>Hello, World!</h1>".html_safe
+
+        message="<h1>Hello, World!</h1>"
+        message+="<p>This is a paragraph</p>"
+        message+="<a href='https://www.google.com'>Google</a>"
+        message+="<img src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png' alt='Google Logo'>"
+        render html: message.html_safe
+    end
+    def getquery
+        message = ""
+        @query=params[:query]
+        message+="<h1>You searched for: #{@query}</h1>"
+
+        message+= "<h1>Controller name is #{params[:controller]}</h1>"
+        # message+= "<h1>Controller name is #{self.class.name}</h1>"
+        message+= "<h1>Action name is #{params[:action]}</h1>"
+        render html: message.html_safe
     end
 end
--- basics/config/routes.rb
+++ basics/config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
 
   get "home" => "home#index"
+  get "home/getquery" => "home#getquery"
 end
