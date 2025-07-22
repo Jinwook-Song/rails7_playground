39a3bb37152b4cec40f8ca7278f18fb16d4bef98
jinwook song <wlsdnr129@naver.com>
2025-07-22 23:00:17 +0900

    feat(employee): create, edit

--- crud_sample/app/controllers/employees_controller.rb
+++ crud_sample/app/controllers/employees_controller.rb
@@ -1,9 +1,44 @@
 class EmployeesController < ApplicationController
+  before_action :set_employee, only: %i[ show edit update ]
+
   def index
     @employees = Employee.all
   end
 
   def show
-    @employee = Employee.find(params[:id])
   end
+
+  def new
+    @employee = Employee.new
+  end
+
+  def create
+    @employee = Employee.new(employee_params)
+
+    if @employee.save
+      redirect_to @employee, notice: "Employee was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @employee.update(employee_params)
+      redirect_to @employee, notice: "Employee was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def set_employee
+      @employee = Employee.find(params[:id])
+    end
+
+    def employee_params
+      params.require(:employee).permit(:firstname, :lastname, :email, :phone, :has_passport, :salary, :notes, :birth_date, :hire_date, :gender)
+    end
 end
